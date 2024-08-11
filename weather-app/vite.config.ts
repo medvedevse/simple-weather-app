ff4c2cbc9bcad13433fe894658b0dcced943b82f
Stepan <stepan.medvedev1995@yandex.ru>
2024-08-11 04:05:40 +0300

    [test-vue] fix

--- weather-app/vite.config.ts
+++ weather-app/vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
-  base: '',
+  base: '/simple-weather-app/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
