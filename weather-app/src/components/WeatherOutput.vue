b8c24c339f92afc6ecff47ec754de26b1f2e8b32
Stepan <stepan.medvedev1995@yandex.ru>
2024-09-13 01:13:23 +0300

    Test-1 Add some styles

--- weather-app/src/components/WeatherOutput.vue
+++ weather-app/src/components/WeatherOutput.vue
@@ -154,6 +154,7 @@ const weatherStore = useWeatherStore();
   display: flex;
   justify-content: space-between;
   .current-day-badge {
+    color: black;
     background-color: #EBF7FF;
     border: 1px solid #16a776;
     border-radius: 10px;
@@ -186,6 +187,7 @@ const weatherStore = useWeatherStore();
   }
 
   .current-day-container:first-child {
+    color: black;
     background-color: #EBF7FF;
     border: 1px solid #16a776;
     border-radius: 10px;
